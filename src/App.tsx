68fe4f4a853d025e88992b13ae14bba3b0c5a937
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-08-22 16:30:47 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+    <div className="w-full min-h-screen">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Navbar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Hambruger from "./assets/Hamburger.svg";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
