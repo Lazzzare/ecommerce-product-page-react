31bea4c1879324694b3fd25ac3d3de5dfcb7dedb
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-08-26 02:50:52 +0400

    Update ProductImageContent.tsx

--- src/ProductImageContent.tsx
+++ src/ProductImageContent.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ProductTextContent from "./ProductTextContent";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 interface Product {
   id: number;
@@ -9,35 +10,82 @@ interface Product {
 
 interface ProductProps {
   products: Product[];
+  slideIndex: number;
+  setSlideIndex: (e: number) => void;
+  nextSlide: () => void;
+  previousSlide: () => void;
+  setShowBigImage: (e: boolean) => void;
 }
 
-const ProductImageContent = ({ products }: ProductProps) => {
+const ProductImageContent = ({
+  products,
+  slideIndex,
+  nextSlide,
+  previousSlide,
+  setShowBigImage,
+}: ProductProps) => {
   const [value, setValue] = useState(0);
-  const { image } = products[value];
+  // const { image } = products[value];
+
+  console.log(slideIndex);
 
   return (
-    <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 lg:mt-10">
+    <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 lg:py-10 lg:place-items-center">
       <article>
         <div>
-          <img src={image} alt="image" className="w-full lg:rounded-2xl" />
+          {products.map((item, index) => (
+            <div
+              key={index}
+              className={slideIndex === index + 1 ? "relative" : "hidden"}
+            >
+              <img
+                src={item.image}
+                alt="image"
+                className="w-full lg:rounded-2xl cursor-pointer"
+                onClick={() => setShowBigImage(true)}
+              />
+
+              <ul className="lg:hidden">
+                <li>
+                  <button
+                    onClick={nextSlide}
+                    className="bg-white rounded-full p-5 shadow absolute right-4 top-1/2 -translate-y-1/2"
+                  >
+                    <FaChevronRight />
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={previousSlide}
+                    className="bg-white rounded-full p-5 shadow absolute left-4 top-1/2 -translate-y-1/2"
+                  >
+                    <FaChevronLeft />
+                  </button>
+                </li>
+              </ul>
+            </div>
+          ))}
         </div>
         <div>
           <ul className="hidden lg:flex items-center justify-start gap-5 flex-wrap mt-5">
-            {products.map((item, index) => (
-              <li
-                key={item.id}
-                onClick={() => setValue(index)}
-                className={`${
-                  index === value && "border-2 border-orange-400 opacity-80"
-                } border-2 rounded-2xl overflow-hidden cursor-pointer`}
-              >
-                <img
-                  src={item.thumbnail}
-                  alt="ImageThumbnail"
-                  className="w-20"
-                />
-              </li>
-            ))}
+            {products.map((item, index) => {
+              console.log(index);
+              return (
+                <li
+                  key={item.id}
+                  onClick={() => setValue(index)}
+                  className={`${
+                    index === value && "border-2 border-orange-400 opacity-80"
+                  } border-2 rounded-2xl overflow-hidden cursor-pointer`}
+                >
+                  <img
+                    src={item.thumbnail}
+                    alt="ImageThumbnail"
+                    className="w-20"
+                  />
+                </li>
+              );
+            })}
           </ul>
         </div>
       </article>
