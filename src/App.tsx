78d4bed0417bd0777713c9312d7864801848b1ba
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-08-26 02:50:45 +0400

    add slider

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,51 @@ import Header from "./Header.js";
 import { ProductData } from "./ProductData.ts";
 import { useState } from "react";
 import ProductImageContent from "./ProductImageContent.tsx";
+import LightContainer from "./LightContainer.tsx";
 
 const App = () => {
   const [products] = useState(ProductData);
+  const [slideIndex, setSlideIndex] = useState(1);
+  const [showBigImage, setShowBigImage] = useState(false);
+
+  const nextSlide = () => {
+    if (slideIndex !== products.length) {
+      setSlideIndex(slideIndex + 1);
+    } else if (slideIndex === products.length) {
+      setSlideIndex(1);
+    }
+  };
+
+  const previousSlide = () => {
+    if (slideIndex !== 1) {
+      setSlideIndex(slideIndex - 1);
+    } else if (slideIndex === 1) {
+      setSlideIndex(products.length);
+    }
+  };
+
+  console.log(showBigImage);
 
   return (
     <div>
       <Header products={products} />
-      <ProductImageContent products={products} />
+      {showBigImage && (
+        <LightContainer
+          products={products}
+          slideIndex={slideIndex}
+          nextSlide={nextSlide}
+          previousSlide={previousSlide}
+          setShowBigImage={setShowBigImage}
+        />
+      )}
+      <ProductImageContent
+        products={products}
+        slideIndex={slideIndex}
+        setSlideIndex={setSlideIndex}
+        nextSlide={nextSlide}
+        previousSlide={previousSlide}
+        setShowBigImage={setShowBigImage}
+      />
     </div>
   );
 };
