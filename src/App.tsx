04ddcf231cd49a79faca01a3f2eb2d8892518441
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-08-25 19:12:35 +0400

    Delete folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import Navbar from "./Navbar";
-import Product from "./Product";
-
 const App = () => {
-  return (
-    <div className="w-full min-h-screen">
-      <Navbar />
-      <Product
-        subtitle="Sneaker Company"
-        title="Fall Limited Edition Sneakers"
-        content="These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer."
-      />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
