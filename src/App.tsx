543d0ba66910b72c975b6b8d0a1be0793dc1b2c3
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-08-25 20:13:11 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import Header from "./Header.js";
+import { ProductData } from "./ProductData.tsx";
+import { useState } from "react";
+
 const App = () => {
-  return <div></div>;
+  const [products] = useState(ProductData);
+  console.log(products);
+
+  return (
+    <div>
+      <Header products={products} />
+
+      <section>
+        {products.map((item) => (
+          <article key={item.id}>
+            <img src={item.image} alt="MainImage" />
+
+            <ul>
+              <img src={item.thumbnail} alt="ImageThumbnail" />
+            </ul>
+          </article>
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default App;
