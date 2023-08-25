0fd99cf91048f82d3d85329a32b285374f2420da
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-08-25 20:13:41 +0400

    Create ProductData.tsx

---
+++ src/ProductData.tsx
@@ -0,0 +1,31 @@
+import ProductImage1 from "./images/image-product-1.jpg";
+import ProductImage1Thumbnail from "./images/image-product-1-thumbnail.jpg";
+import ProductImage2 from "./images/image-product-2.jpg";
+import ProductImage2Thumbnail from "./images/image-product-2-thumbnail.jpg";
+import ProductImage3 from "./images/image-product-3.jpg";
+import ProductImage3Thumbnail from "./images/image-product-3-thumbnail.jpg";
+import ProductImage4 from "./images/image-product-4.jpg";
+import ProductImage4Thumbnail from "./images/image-product-4-thumbnail.jpg";
+
+export const ProductData = [
+  {
+    id: 1,
+    image: ProductImage1,
+    thumbnail: ProductImage1Thumbnail,
+  },
+  {
+    id: 2,
+    image: ProductImage2,
+    thumbnail: ProductImage2Thumbnail,
+  },
+  {
+    id: 3,
+    image: ProductImage3,
+    thumbnail: ProductImage3Thumbnail,
+  },
+  {
+    id: 4,
+    image: ProductImage4,
+    thumbnail: ProductImage4Thumbnail,
+  },
+];
